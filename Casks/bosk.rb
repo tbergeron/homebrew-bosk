43f9365f5fcf0b5386b565199c7ff66b7156a59b
Tommy Bergeron <tom@brainpad.org>
2026-09-25 14:47:21 -0400

    Bosk 0.15.2

--- Casks/bosk.rb
+++ Casks/bosk.rb
@@ -1,6 +1,6 @@
 cask "bosk" do
-  version "0.15.1"
-  sha256 "1a5766365d119c4a4b421a1a2bb8fda4382bdddcb71fae333e68f41d5b5fb72e"
+  version "0.15.2"
+  sha256 "1b528539e28cea5a423df6f5ad0d63b17d3cc5a4c980a26451106e458a2c65ca"
 
   url "https://github.com/tbergeron/bosk-browser/releases/download/v#{version}/Bosk-#{version}.dmg"
   name "Bosk"
